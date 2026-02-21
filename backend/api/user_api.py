ea4ec56956093969f1f83d3900eb71c5a8e571ce
Oleksandr Dimbrovskyi <sanya.dimbrovskyi@gmail.com>
2026-02-21 05:25:11 -0600

    Developed api route to retrieve user's resume by email

--- backend/api/user_api.py
+++ backend/api/user_api.py
@@ -3,6 +3,7 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from backend.auth.auth import get_password_hash, verify_password, create_access_token
 from backend.dependencies.user_dependencies import get_user_by_email, create_user, get_email_from_token
 from backend.database.session import get_db
+from backend.models.candidate_model import CandidateResume
 from sqlalchemy.orm import Session
 from backend.schemas.user_schemas import UserCreate, UserOut
 
@@ -53,3 +54,30 @@ def get_me(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     user = get_user_by_email(db, data)
 
     return user
+
+
+@router.get("/my_resume/")
+async def get_resume(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    email = get_email_from_token(token)
+
+    resume = db.query(CandidateResume).filter(CandidateResume.user_email == email).first()
+
+    if not resume:
+        return {"error": "Resume not found for this user."}
+
+    resume_data = {
+        "profile_name": resume.profile_name,
+        "user_email": resume.user_email,
+        "mobile_number": resume.mobile_number,
+        "designation": resume.designation,
+        "total_experience": resume.total_experience,
+        "education": resume.education,
+        "skills": resume.skills,
+        "company_names": resume.company_names,
+        "ai_summary": resume.ai_summary,
+        "ai_strengths": resume.ai_strengths,
+        "experiences": resume.experiences,
+        "resume_json": resume.resume_json
+    }
+
+    return resume_data
