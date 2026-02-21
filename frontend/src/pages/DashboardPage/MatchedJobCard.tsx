b8d947b3da859602791d224cc124d5e687165ede
Yaarik16 <yarikdimbrovsky@gmail.com>
2026-02-20 22:43:06 -0600

    mock dashboard jobs outline

---
+++ frontend/src/pages/DashboardPage/MatchedJobCard.tsx
@@ -0,0 +1,59 @@
+import styles from "./MatchedJobCard.module.scss";
+import {MapPin} from "lucide-react"
+
+interface MatchedJob {
+    title: string,
+    shortDescription: string,
+    companyName: string,
+    location: string,
+    onSite: boolean,
+    skills: string[],
+    salary: string,
+    matchRate: number,
+}
+
+interface MatchedJobCardProps {
+    job: MatchedJob;
+    index: number;
+}
+
+const MatchedJobCard = ({job, index}: MatchedJobCardProps) => {
+    return (
+        <div className={styles.job} key={index}>
+            <div className={styles.mainInfo}>
+                <div className={styles.info}>
+                    <div className={styles.titleInfo}>
+                        <span className={styles.title}>{job.title}</span>
+                        <span className={styles.matchRate}>{job.matchRate}% match</span>
+
+                    </div>
+                    <div className={styles.companyInfo}>
+                        <span className={styles.companyName}>{job.companyName}</span>
+                        <span className={styles.location}><MapPin size={14}/>{job.location}</span>
+                        <span className={styles.onSite}>{job.onSite ? "On site" : "Remote"}</span>
+                    </div>
+                </div>
+                <div className={styles.salarySection}>
+                    <p className={styles.salaryText}>Salary range</p>
+                    <p className={styles.salary}>{job.salary}</p>
+                </div>
+            </div>
+            <div className={styles.body}>
+                <p className={styles.description}>{job.shortDescription}</p>
+                <div className={styles.skills}>
+                    {job.skills.map((skill, index) => (
+                        <div className={styles.skill} key={index}>
+                            {skill}
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className={styles.btns}>
+                <button className={styles.detailsBtn}>View details</button>
+                <button className={styles.saveBtn}>Save</button>
+            </div>
+        </div>
+    );
+};
+
+export default MatchedJobCard;
